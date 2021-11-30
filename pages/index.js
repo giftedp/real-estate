33751ae1ab02c2479f85c2b5c0c50bf0a8b7fc67
Gift Mvula <mvulagp@gmail.com>
2021-11-30 16:16:43 +0200

    second commit

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import { baseUrl, fetchApi } from '../utils/fetchApi';
 
 export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
   <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
-    <Image src={imageUrl} width={500} height={300} />
+    <Image src={imageUrl} width={500} height={300} alt="" />
     <Box p='5'>
       <Text color='gray.500' fontSize='sm' fontWeight='medium'>{purpose}</Text>
       <Text fontSize='3xl' fontWeight='bold'>{title1}<br />{title2}</Text>
@@ -30,7 +30,7 @@ const Home = ({ propertiesForSale, propertiesForRent }) => (
       desc2='and more'
       buttonText='Explore Renting'
       linkName='/search?purpose=for-rent'
-      imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
+      imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4' alt=""
     />
     <Flex flexWrap='wrap'>
       {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
@@ -43,7 +43,7 @@ const Home = ({ propertiesForSale, propertiesForRent }) => (
       desc2=' villas and more'
       buttonText='Explore Buying'
       linkName='/search?purpose=for-sale'
-      imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
+      imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008' alt=""
     />
     <Flex flexWrap='wrap'>
       {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
